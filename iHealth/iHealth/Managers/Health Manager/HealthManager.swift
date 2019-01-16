5aed5211a6b4b716ae3783d25f672893e339e75f
Ricardo Casanova <ricardo@pijp.eu>
2019-01-16 12:53:14 +0100

    Add initial requestAuthorization method

--- iHealth/iHealth/Managers/Health Manager/HealthManager.swift
+++ iHealth/iHealth/Managers/Health Manager/HealthManager.swift
@@ -7,9 +7,30 @@
 //
 
 import Foundation
+import HealthKit
+
+typealias HealthAuthCompletionBlock = (_ success: Bool, _ error: Error?) -> Void
 
 class HealthManager: NSObject {
     
     static let shared: HealthManager = { return HealthManager() }()
+    
+    public func requestAuthorization(completion: @escaping HealthAuthCompletionBlock) {
+        guard HKHealthStore.isHealthDataAvailable() else {
+            completion(false, nil)
+            return
+        }
+        
+        guard   let running = HKObjectType.quantityType(forIdentifier: .distanceWalkingRunning) else {
+                completion(false, nil)
+                return
+        }
+        
+        let healthKitTypes: Set<HKSampleType> = [running]
+        
+        HKHealthStore().requestAuthorization(toShare: healthKitTypes, read: healthKitTypes) { (success, error) in
+            completion(success, error)
+        }
+    }
 
 }
