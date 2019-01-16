3c6653b9a25252b641c5019d6af56e3663062f46
Ricardo Casanova <ricardo@pijp.eu>
2019-01-16 13:00:46 +0100

    Add HealthDelegate protocol

--- iHealth/iHealth/Managers/Health Manager/HealthManager.swift
+++ iHealth/iHealth/Managers/Health Manager/HealthManager.swift
@@ -9,28 +9,58 @@
 import Foundation
 import HealthKit
 
-typealias HealthAuthCompletionBlock = (_ success: Bool, _ error: Error?) -> Void
-
 class HealthManager: NSObject {
     
+    let store: HKHealthStore
     static let shared: HealthManager = { return HealthManager() }()
     
-    public func requestAuthorization(completion: @escaping HealthAuthCompletionBlock) {
+    private override init() {
+        self.store = HKHealthStore()
+        super.init()
+    }
+
+}
+
+extension HealthManager: HealthDelegate {
+    
+    func requestAuthorization(completion: @escaping HealthAuthCompletionBlock) {
         guard HKHealthStore.isHealthDataAvailable() else {
             completion(false, nil)
             return
         }
         
         guard   let running = HKObjectType.quantityType(forIdentifier: .distanceWalkingRunning) else {
-                completion(false, nil)
-                return
+            completion(false, nil)
+            return
         }
         
         let healthKitTypes: Set<HKSampleType> = [running]
         
-        HKHealthStore().requestAuthorization(toShare: healthKitTypes, read: healthKitTypes) { (success, error) in
+        store.requestAuthorization(toShare: healthKitTypes, read: healthKitTypes) { (success, error) in
             completion(success, error)
         }
     }
-
+    
+    func getSteps(completion: @escaping HealthGetStepsCompletionBlock) {
+        guard let startDate = Calendar.current.date(bySettingHour: 0, minute: 0, second: 0, of: Date()) else {
+            completion(0, false, nil)
+            return
+        }
+        
+        let endDate = Date()
+        
+        print("Collecting workouts between \(startDate) and \(endDate)")
+        
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: HKQueryOptions.strictEndDate)
+        
+        let query = HKSampleQuery(sampleType: HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning)!, predicate: predicate, limit: HKObjectQueryNoLimit, sortDescriptors: nil) { (query, results, error) in
+            for item in results! {
+                print(item)
+            }
+            completion(0, false, nil)
+        }
+        
+        store.execute(query)
+    }
+    
 }
---
+++ iHealth/iHealth/Managers/Health Manager/HealthProtocols.swift
@@ -0,0 +1,17 @@
+//
+//  HealthProtocols.swift
+//  iHealth
+//
+//  Created by Ricardo Casanova on 16/01/2019.
+//  Copyright © 2019 Pijp. All rights reserved.
+//
+
+import Foundation
+
+typealias HealthAuthCompletionBlock = (_ success: Bool, _ error: Error?) -> Void
+typealias HealthGetStepsCompletionBlock = (_ steps: Int, _ success: Bool, _ error: Error?) -> Void
+
+protocol HealthDelegate: class {
+    func requestAuthorization(completion: @escaping HealthAuthCompletionBlock)
+    func getSteps(completion: @escaping HealthGetStepsCompletionBlock)
+}
